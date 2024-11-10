02610c67141a043ec4ced1338c8366c310672166
filip jakobsson <filja442@student.liu.se>
2024-11-10 11:55:27 +0100

    Initial commit, getting started

--- src/main/java/com/ticketReminder/TicketReminder.java
+++ src/main/java/com/ticketReminder/TicketReminder.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.ticketReminder;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Wilderness Course Ticket Reminder"
 )
-public class ExamplePlugin extends Plugin
+public class TicketReminder extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TicketReminderConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	TicketReminderConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TicketReminderConfig.class);
 	}
 }
